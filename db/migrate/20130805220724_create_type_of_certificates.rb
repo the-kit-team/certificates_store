1d4edaf1b9c91b1dc9047cca201be0f6a7767b9d
Vladislav Petrov <electronicchest@gmail.com>
2013-08-06 02:12:17 +0400

    Add migration join between order and type of certificates

--- db/migrate/20130805220724_create_type_of_certificates.rb
+++ db/migrate/20130805220724_create_type_of_certificates.rb
@@ -1,6 +1,7 @@
 class CreateTypeOfCertificates < ActiveRecord::Migration
   def change
     create_table :type_of_certificates do |t|
+      t.belongs_to :order
       t.string :title
 
       t.timestamps
