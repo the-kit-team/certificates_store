8282cfaae9f7f4c22764a76cd00940c6a8450249
Vladislav Petrov <electronicchest@gmail.com>
2013-08-06 02:15:51 +0400

    Add migration join between order and type of legal entity

--- db/migrate/20130805221320_create_type_of_legal_entities.rb
+++ db/migrate/20130805221320_create_type_of_legal_entities.rb
@@ -1,6 +1,7 @@
 class CreateTypeOfLegalEntities < ActiveRecord::Migration
   def change
     create_table :type_of_legal_entities do |t|
+      t.belongs_to :order
       t.string :title
 
       t.timestamps
