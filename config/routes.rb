2ce6104fb5d1f97983558c6c429ac513a82c60a9
Vladislav Petrov <electronicchest@gmail.com>
2013-08-06 05:35:36 +0400

    Change index to home
    rails g controller Home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 CertificatesStore::Application.routes.draw do
+  get "home/index"
   resources :orders
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index', as: 'home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
