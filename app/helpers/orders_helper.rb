09531028df2cf993e72a5fe722f4c213effb35c9
Vladislav Petrov <electronicchest@gmail.com>
2013-08-08 06:35:36 +0400

    Add background for certificate, and change style of fill certificate

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,6 +2,8 @@
 module OrdersHelper
   def generate_pdf order
     Prawn::Document.new do
+      image 'lib/certificate_backgrounds/obrazec_big.jpg', at: [-36,755], width: 620, height: 795                               
+
       font_families.update(
         "Verdana" => {
           bold: "lib/fonts/verdanab.ttf",
@@ -9,31 +11,16 @@ module OrdersHelper
           normal: "lib/fonts/verdana.ttf" 
         })
       font "Verdana"
+      fill_color 'ff0000'
 
-      text "Сертификат #{TypeOfCertificate.find(order.type_of_certificate_id).title}", align: :center, style: :bold
-      text ""
-      text "Тип юридического лица: #{TypeOfLegalEntity.find(order.type_of_legal_entity_id).title}"
-      text "Наименование компании: #{order.company}"
-      text "в лице: #{order.creator_name}"
-      text "Юридический адресс: #{order.registered_address}"
-      text "Фактический адресс: #{order.actual_address}"
-      text "Адресс на английском: #{order.address_on_english}"
-      text "Телефоннный номер: #{order.phone}"
-      text "Номер факса: #{order.fax}"
-      text "Адресс электропочты: #{order.email}"
-      text "ИНН: #{order.inn}"
-      text "КПП: #{order.kpp}"
-      text "ОГРН: #{order.ogrn}"
-      text "Наименование банка: #{order.bank}"
-      text "Номер счёта: #{order.current_account}"
-      text "Корреспонденский счёт: #{order.correspondent_account}"
-      text "БИК: #{order.bik}"
-      text "Представитель банка: #{order.bank_person}"
-      text "Имена аудиторов: #{order.auditors_names}"
-    
-      text "Перечень работ:"
+      text_box "#{TypeOfCertificate.find(order.type_of_certificate_id).title}", size: 14, at: [-36,470], align: :center, width: 620
+      text_box "#{TypeOfLegalEntity.find(order.type_of_legal_entity_id).full_title}", size: 14, at: [-36,450], align: :center, width: 620
+      text_box "Наименование компании: #{order.company}", size: 14, at: [-36,430], align: :center, width: 620
+
+      f = 0
       order.list_of_works_categories.map(&:title).each do |category|
-        text "   #{category}"
+        text_box "#{category}", size: 10, at: [40,355-f], width: 400
+        f += 20
       end
     end.render
   end
