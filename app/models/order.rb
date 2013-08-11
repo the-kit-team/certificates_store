f6a3f1ba03adb0f8dbdf3297011151797c5df145
Vladislav Petrov <electronicchest@gmail.com>
2013-08-11 09:54:34 +0400

    Revert "Fix bag model order"
    This reverts commit c13eed1f01783385027c09cafbf4941b6b940597.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,3 @@
 class Order < ActiveRecord::Base
-  belongs_to :type_of_certificate
-  belongs_to :type_of_legal_entity
-  belongs_to :status
   has_and_belongs_to_many :list_of_works_categories
 end
