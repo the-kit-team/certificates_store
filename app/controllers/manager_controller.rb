e06f32e3d87426af0be32de3aad9f6d57216359e
Vladislav Petrov <electronicchest@gmail.com>
2013-08-15 04:45:12 +0400

    Add find option to manager page
    Can search for column type_of_legal_entity

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -1,7 +1,7 @@
 class ManagerController < ApplicationController
   include ApplicationHelper
   
-  before_action :check_permissions
+  skip_before_action :authorize if (:permission == 'manager' or :permission == 'admin')
   
   # GET /manager
   # GET /manager?status=1
@@ -20,7 +20,10 @@ class ManagerController < ApplicationController
               @orders.select { |e| e.phone        =~ /#{params[:word]}/ } |
               @orders.select { |e| e.fax          =~ /#{params[:word]}/ } |
               @orders.select { |e| e.creator_name =~ /#{params[:word]}/ } |
-              @orders.select { |e| e.email        =~ /#{params[:word]}/ }
+              @orders.select { |e| e.email        =~ /#{params[:word]}/ } |
+              @orders.select { |e| 
+                TypeOfLegalEntity.find(e.type_of_legal_entity_id).title =~ /#{params[:word].mb_chars.upcase.to_s}/ # mb_char - for russian language
+              }
     render template: "manager/index"
   end
   
@@ -28,9 +31,5 @@ class ManagerController < ApplicationController
   
     def manager_params
       params.require(:manager).permit(:word)
-    end
-    
-    def check_permissions
-      redirect_to home_path if permission != 'manager'
     end
 end
