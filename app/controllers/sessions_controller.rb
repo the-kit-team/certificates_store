4bd5d189835f747813da964b32f2cf4752d9b781
Vladislav Petrov <electronicchest@gmail.com>
2013-08-16 12:54:35 +0400

    Add email to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,12 +9,14 @@ class SessionsController < ApplicationController
     
     if user and user.authenticate(params[:password])
       session[:user_id] = user.id
+      session[:user_email] = user.email
     else
       redirect_to login_url, alert: "Invalid user/password combination" end
   end
 
   def destroy
     session[:user_id] = nil
+    session[:user_email] = nil
     redirect_to home_url, notice: "Logged out"
   end
 end
