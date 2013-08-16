e5b41a27bbbb2a453f54d55cac8c83aa2ba05edf
Anton Petrov <givemeletter@gmail.com>
2013-08-17 01:17:29 +0400

    Cleanup current_user method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,5 @@
 module ApplicationHelper
-  @current_user
-  @latest_user_id
-
   def current_user
-    unless @latest_user_id == session[:user_id]
-      @current_user = User.find_by_id(session[:user_id])
-      @latest_user_id = session[:user_id]
-    end
-    @current_user
+    @current_user ||= User.find_by_id(session[:user_id])
   end
 end
