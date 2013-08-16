fc77510c86e6513296126993e369295e66098289
Vladislav Petrov <electronicchest@gmail.com>
2013-08-16 15:02:40 +0400

    Merge fails delete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,9 @@
 module ApplicationHelper
-<<<<<<< HEAD
-  @current_user
-  @last_user_id
-  
-  def user_is permission
-    permission && @current_user
-  end
-  
-  def set_current_user
-    if (not @last_user_id == session[:user_id]) or @last_user_id == nil
-      @last_user_id = session[:user_id]
-      @current_user = User.find(session[:user_id]).permission.title
-    end
-=======
   def permission
-    Permission.find_by_id(current_user.permission_id).title rescue false
+    current_user.permission rescue false
   end
 
   def current_user
     User.find_by_id(session[:user_id])
->>>>>>> fa6586db17ca8a7adebc1e5247d72a27d93ca57f
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ActiveRecord::Base
   belongs_to :permission
-<<<<<<< HEAD
-  validates :email, presence: true, uniqueness: true
-=======
->>>>>>> fa6586db17ca8a7adebc1e5247d72a27d93ca57f
   has_secure_password
   validates :email, presence: true, uniqueness: true
 
