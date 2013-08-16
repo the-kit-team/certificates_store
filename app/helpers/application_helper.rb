ce853f8c3f8bb2dbe6d6f5fe3fb3c743865c00c3
Vladislav Petrov <electronicchest@gmail.com>
2013-08-16 12:55:46 +0400

    Refactoring permissions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
-  def permission
-    Permission.find_by_id(User.find_by_id(session[:user_id]).permission_id).title rescue false
+  @current_user
+  @last_user_id
+  
+  def user_is permission
+    permission && @current_user
+  end
+  
+  def set_current_user
+    if (not @last_user_id == session[:user_id]) or @last_user_id == nil
+      @last_user_id = session[:user_id]
+      @current_user = User.find(session[:user_id]).permission.title
+    end
   end
 end
