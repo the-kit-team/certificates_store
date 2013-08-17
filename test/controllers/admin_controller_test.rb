cf19202173111b40fedced400328e0ee427cd2de
Anton Petrov <givemeletter@gmail.com>
2013-08-17 13:45:33 +0400

    Made the test authorization aware

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "should redirect to login page unless logged in as admin" do
     get :index
-    assert_response :success
+    assert_redirected_to login_url
   end
 
 end
