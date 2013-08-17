c0dda9bd10c33b25e2348d2f022284b42b45feaa
Anton Petrov <givemeletter@gmail.com>
2013-08-17 17:33:55 +0400

    Manager#index test now passes

--- test/controllers/manager_controller_test.rb
+++ test/controllers/manager_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class ManagerControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "should get index if logged in as manager" do
+    login_as(:manager)
     get :index
     assert_response :success
+  end
+
+  test "should redirect to login unless logged in as admin or manager" do
+    get :index
+    assert_redirected_to login_url
   end
 
   test "should get show" do
