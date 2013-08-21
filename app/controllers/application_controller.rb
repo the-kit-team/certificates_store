fe402bbe3968f1cfebc4159ce0ef6c45924d32ea
Vladislav Petrov <electronicchest@gmail.com>
2013-08-21 17:33:22 +0400

    Add default CanCan function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,4 +30,8 @@ class ApplicationController < ActionController::Base
         redirect_to login_url, notice: "Please log in" if not logout_workplace
       end
     end
+    
+    def current_ability
+      @current_ability ||= Ability.new(current_user)
+    end
 end
