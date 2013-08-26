48fc339742af908fcc6880be82b1bd0fd6494718
Vladislav Petrov <electronicchest@gmail.com>
2013-08-27 00:55:43 +0400

    Refactoring - delete brackets(curve, parenthisis)

--- app/mailers/main_mailer.rb
+++ app/mailers/main_mailer.rb
@@ -4,6 +4,6 @@ class MainMailer < ActionMailer::Base
   def welcome_email(user)
     @user = user
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail to: @user.email, subject: 'Welcome to My Awesome Site'
   end
 end
