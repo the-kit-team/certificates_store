5765cf055269c1b2572bd788a688f3848276e443
Vladislav Petrov <electronicchest@gmail.com>
2013-08-27 00:28:44 +0400

    MainMailer#welcome_email test - not pass
    Can't create welcome_email fixture

--- test/mailers/main_mailer_test.rb
+++ test/mailers/main_mailer_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class MainMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  tests MainMailer
+  
+    test "welcome email is shold to delivered " do
+      email = MainMailer.welcome_email(users(:client)).deliver
+      
+      assert !ActionMailer::Base.deliveries.empty?
+
+      assert_equal ['notifications@example.com'], email.from
+      assert_equal [users(:client).email], email.to
+      assert_equal 'Welcome to My Awesome Site', email.subject
+      assert_equal read_fixture('welcome_email').join, email.body.to_s
+    end
 end
