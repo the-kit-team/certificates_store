425f048aefc3011574adfc36ad9935f15e686dbe
Vladislav Petrov <electronicchest@gmail.com>
2013-08-27 03:38:50 +0400

    User#create -> welcome_email test is pass

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -56,4 +56,17 @@ class UsersControllerTest < ActionController::TestCase
 
     assert_redirected_to users_url
   end
+  
+  test "should send welcome email after creating new user" do
+    post :create, user: { email: 'unique_email', password: 'qwerty', password_confirmation: 'qwerty' }
+
+    welcome_email = ActionMailer::Base.deliveries.last
+    user = assigns(:user)
+    
+    assert_equal ['notifications@example.com'], welcome_email.from
+    assert_equal user.email, welcome_email.to[0]
+    assert_equal 'Welcome to My Awesome Site', welcome_email.subject
+    assert_match /Welcome to example.com, unique_email/, welcome_email.text_part.body.to_s
+    assert_match /<h1>Welcome to example.com, unique_email<\/h1>/, welcome_email.html_part.body.to_s
+  end
 end
