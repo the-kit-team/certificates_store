5aa3ba2846f0cbe527e5548ba611d71fd5442016
Vladislav Petrov <electronicchest@gmail.com>
2013-08-27 00:30:29 +0400

    Add smtp configure for gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,17 @@ CertificatesStore::Application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  # Mailer configure for GMail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'username',
+    password:             'password',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
