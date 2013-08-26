5a9d92d957dc223a403c0628d30c2072eb7f5ee7
Vladislav Petrov <electronicchest@gmail.com>
2013-08-27 01:59:46 +0400

    Refactoring - 'assert !' = 'assert_not'

--- test/mailers/main_mailer_test.rb
+++ test/mailers/main_mailer_test.rb
@@ -6,7 +6,7 @@ class MainMailerTest < ActionMailer::TestCase
     test "welcome email is shold to delivered " do
       email = MainMailer.welcome_email(users(:client)).deliver
       
-      assert !ActionMailer::Base.deliveries.empty?
+      assert_not ActionMailer::Base.deliveries.empty?
 
       assert_equal ['notifications@example.com'], email.from
       assert_equal [users(:client).email], email.to
