c778764cd882963ee5e1db7c92f1d44ba3778380
Vladislav Petrov <electronicchest@gmail.com>
2013-08-27 22:27:12 +0400

    Tests/MainMailer add test invoice email

--- test/mailers/main_mailer_test.rb
+++ test/mailers/main_mailer_test.rb
@@ -14,4 +14,16 @@ class MainMailerTest < ActionMailer::TestCase
       assert_equal read_fixture('welcome_email.txt').join, email.text_part.body.to_s
       assert_equal read_fixture('welcome_email.html').join, email.html_part.body.to_s
     end
+    
+    test "invoice email is shold to delivered " do
+      email = MainMailer.invoice(orders(:kennys_order)).deliver
+      
+      assert_not ActionMailer::Base.deliveries.empty?
+
+      assert_equal ['notifications@example.com'], email.from
+      assert_equal [orders(:kennys_order).email], email.to
+      assert_equal 'Thank you for chose our resource', email.subject
+      assert_equal read_fixture('invoice_email.txt').join, email.text_part.body.to_s
+      assert_equal read_fixture('invoice_email.html').join, email.html_part.body.to_s
+    end
 end
