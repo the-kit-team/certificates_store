c6211414ae2cabf10f548b233fdf402a75ec9e36
Vladislav Petrov <electronicchest@gmail.com>
2013-08-27 08:48:23 +0400

    Home#index - modify cache method
    Less 1 ms

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,6 @@ class HomeController < ApplicationController
   skip_before_action :authorize
   
   def index
-    last_modified = File.mtime("#{Rails.root}/app/views/home/index.html.erb")
-    fresh_when last_modified: last_modified , public: true, etag: last_modified
+    fresh_when session[:user_email]
   end
 end
