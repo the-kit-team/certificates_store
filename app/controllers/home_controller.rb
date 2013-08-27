a5ac1e3fbb4f365ca4470b29dadb5c6e81c28ed1
Vladislav Petrov <electronicchest@gmail.com>
2013-08-27 08:02:34 +0400

    Home#index - trying cache

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   skip_before_action :authorize
   
   def index
+    last_modified = File.mtime("#{Rails.root}/app/views/home/index.html.erb")
+    fresh_when last_modified: last_modified , public: true, etag: last_modified
   end
 end
