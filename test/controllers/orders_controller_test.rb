17f32733a222fcf75d641ae1a328f75a18001319
Vladislav Petrov <electronicchest@gmail.com>
2013-08-28 23:20:49 +0400

    Orders#after create invoice test - refactoring

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -55,15 +55,19 @@ class OrdersControllerTest < ActionController::TestCase
   end
   
   test "should send invoice email after creating new order" do
-    post :create, order: { type_of_certificate_id: @order.type_of_certificate_id, type_of_legal_entity_id: @order.type_of_legal_entity_id, actual_address: @order.actual_address, address_on_english: @order.address_on_english, auditors_names: @order.auditors_names, bank: @order.bank, bank_person: @order.bank_person, bik: @order.bik, company: @order.company, correspondent_account: @order.correspondent_account, creator_name: @order.creator_name, current_account: @order.current_account, email: @order.email, fax: @order.fax, inn: @order.inn, kpp: @order.kpp, ogrn: @order.ogrn, phone: @order.phone, registered_address: @order.registered_address, list_of_works_category_ids: @order.list_of_works_category_ids }
-
-    welcome_email = ActionMailer::Base.deliveries.last
+    assert_difference("MainMailer.deliveries.count") do
+      assert_difference('Order.count') do
+        post :create, order: { type_of_certificate_id: @order.type_of_certificate_id, type_of_legal_entity_id: @order.type_of_legal_entity_id, actual_address: @order.actual_address, address_on_english: @order.address_on_english, auditors_names: @order.auditors_names, bank: @order.bank, bank_person: @order.bank_person, bik: @order.bik, company: @order.company, correspondent_account: @order.correspondent_account, creator_name: @order.creator_name, current_account: @order.current_account, email: @order.email, fax: @order.fax, inn: @order.inn, kpp: @order.kpp, ogrn: @order.ogrn, phone: @order.phone, registered_address: @order.registered_address, list_of_works_category_ids: @order.list_of_works_category_ids }
+      end
+    end
+    
+    invoice_email = MainMailer.deliveries.last
     order = assigns(:order)
     
-    assert_equal ['support@licenziyaplus.ru'], welcome_email.from
-    assert_equal @order.email, welcome_email.to[0]
-    assert_equal "Спасибо за заказ ##{order.id}", welcome_email.subject
-    assert_match /Здравствуйте, #{order.creator_name}/, welcome_email.text_part.body.to_s
-    assert_match /Здравствуйте, #{order.creator_name}/, welcome_email.html_part.body.to_s
+    assert_equal ['support@licenziyaplus.ru'], invoice_email.from
+    assert_equal @order.email, invoice_email.to[0]
+    assert_equal "Спасибо за заказ ##{order.id}", invoice_email.subject
+    assert_match /Здравствуйте, #{order.creator_name}/, invoice_email.text_part.body.to_s
+    assert_match /Здравствуйте, #{order.creator_name}/, invoice_email.html_part.body.to_s
   end
 end
